fad5bbec0bad2b9c4aab94e97e24956626661b32
Elliot <elliot.one.harding@gmail.com>
2019-10-30 18:19:31 +0000

    Progress

---
+++ ASE/ASE/BST.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+class BST
+{
+public:
+	using Key = int;
+	using Item = std::string;
+
+	BST();
+	~BST();
+	BST(const BST&);
+
+	void insert(Key, Item);
+	Item* lookup(Key);
+	void displayTree();
+	void remove();
+
+private:
+	struct Node;
+	Node* root;
+
+	static bool isLeaf(Node*);
+	static Item* lookupRec(Key, Node*);
+	static void insertRec(Key, Item, Node*&);
+	static void deepDelete(Node*&);
+	static Node* deepCopy(Node*);
+	void displayEntryRec(std::ostream& os, Node* n) const;
+	void displayTreeRec(Node* n, std::string index = "");
+	void displayEntries(std::ostream& os) const;	
+
+	friend std::ostream& operator<<(std::ostream&, const BST&);
+};
+
+std::ostream& operator<<(std::ostream&, const BST&);
+
